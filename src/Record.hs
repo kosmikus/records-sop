6861cba2238e2934bd1194f9d9e85d7b41d705f3
Andres Loeh <andres@well-typed.com>
2016-09-12 15:43:31 +0200

    Documentation.

--- src/Record.hs
+++ src/Record.hs
@@ -1,10 +1,40 @@
-{-# LANGUAGE TypeInType, TypeFamilies, GADTs, DataKinds, PolyKinds, TypeOperators #-}
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables #-}
-{-# LANGUAGE StandaloneDeriving, FlexibleContexts, UndecidableInstances #-}
-{-# LANGUAGE DeriveGeneric, DuplicateRecordFields #-}
-{-# LANGUAGE RankNTypes, ConstraintKinds #-}
-{-# LANGUAGE UndecidableSuperClasses #-}
-module Record where
+{-# LANGUAGE
+    ConstraintKinds
+  , DeriveGeneric
+  , FlexibleInstances
+  , ScopedTypeVariables
+  , StandaloneDeriving
+  , TypeInType
+  , TypeFamilies
+  , TypeOperators
+  , UndecidableInstances
+#-}
+{-# OPTIONS_GHC
+  -fno-warn-unticked-promoted-constructors
+  -fno-warn-unused-top-binds
+  -fno-warn-redundant-constraints
+#-}
+module Record
+  ( -- * A suitable representation for single-constructor records
+    FieldLabel
+  , RecordCode
+  , Record
+  , RecordRep
+    -- * Computing the record code
+  , RecordCodeOf
+  , IsRecord
+  , ValidRecordCode
+  , ExtractTypesFromRecordCode
+  , ExtractLabelsFromRecordCode
+  , RecombineRecordCode
+    -- * Conversion between a type and its record representation.
+  , toRecord
+  , fromRecord
+    -- * Utilities
+  , P(..)
+  , Snd
+  )
+  where
 
 import Generics.SOP.BasicFunctors
 import Generics.SOP.NP
@@ -12,11 +42,14 @@ import Generics.SOP.NS
 import Generics.SOP.Universe
 import Generics.SOP.Sing
 import Generics.SOP.Type.Metadata
+import qualified GHC.Generics as GHC
 import GHC.TypeLits
 import GHC.Types
 import Unsafe.Coerce
 
--- * A suitable representation for single-constructor records.
+--------------------------------------------------------------------------
+-- A suitable representation for single-constructor records.
+--------------------------------------------------------------------------
 
 -- | On the type-level, we represent fiel labels using symbols.
 type FieldLabel = Symbol
@@ -32,8 +65,13 @@ type FieldLabel = Symbol
 --
 type RecordCode = [(FieldLabel, Type)]
 
+-- | The record representation of a type is a record indexed
+-- by the record code.
+--
+type RecordRep (a :: Type) = Record (RecordCodeOf a)
+
 -- | The representation of a record is just a product indexed by
--- the record code, containing elements of the types indicated
+-- a record code, containing elements of the types indicated
 -- by the code.
 --
 -- Note that the representation is deliberately chosen such that
@@ -42,9 +80,9 @@ type RecordCode = [(FieldLabel, Type)]
 --
 type Record (r :: RecordCode) = NP P r
 
-type RecordRep (a :: Type) = Record (RecordCodeOf a)
-
--- * Computing the record code
+--------------------------------------------------------------------------
+-- Computing the record code
+--------------------------------------------------------------------------
 
 -- | This type-level function takes the type-level metadata provided
 -- by generics-sop as well as the normal generics-sop code, and transforms
@@ -82,64 +120,118 @@ type family ToRecordCode_Field (fis :: [FieldInfo]) (c :: [Type]) :: RecordCode 
   ToRecordCode_Field '[]                    '[]        = '[]
   ToRecordCode_Field ( 'FieldInfo l : fis ) ( t : ts ) = '(l, t) : ToRecordCode_Field fis ts
 
--- * Compatibility of the record code and the original code.
+-- * Relating the record code and the original code.
+
+-- | The constraint @IsRecord a r@ states that the type 'a' is a record type
+-- (i.e., has exactly one constructor and field labels) and that 'r' is the
+-- record code associated with 'a'.
 --
--- If things are correct, the generics-sop code and the record code are strongly
--- related. Stripping all the field names from the record code should give us the
--- original code.
+type IsRecord (a :: Type) (r :: RecordCode) =
+  IsRecord' a r (GetSingleton (Code a))
 
+-- | The constraint @IsRecord' a r xs@ states that 'a' is a record type
+-- with record code 'r', and that the types contained in 'r' correspond
+-- to the list 'xs'.
+--
+-- If the record code computation is correct, then the record code of a
+-- type is strongly related to the original generics-sop code. Extracting
+-- the types out of 'r' should correspond to 'xs'. Recombining the
+-- labels from 'r' with 'xs' should yield 'r' exactly. These sanity
+-- properties are captured by 'ValidRecordCode'.
+--
+type IsRecord' (a :: Type) (r :: RecordCode) (xs :: [Type]) =
+  ( Generic a, Code a ~ '[ xs ]
+  , RecordCodeOf a ~ r, ValidRecordCode r xs
+  )
+
+-- | Relates a recordcode 'r' and a list of types 'xs', stating that
+-- 'xs' is indeed the list of types contained in 'r'.
+--
+type ValidRecordCode (r :: RecordCode) (xs :: [Type]) =
+  ( ExtractTypesFromRecordCode r ~ xs
+  , RecombineRecordCode (ExtractLabelsFromRecordCode r) xs ~ r
+  )
+
+-- | Extracts all the types from a record code.
 type family ExtractTypesFromRecordCode (r :: RecordCode) :: [Type] where
-  ExtractTypesFromRecordCode '[]               = '[]
-  ExtractTypesFromRecordCode ( '(_, a) : sig ) = a : ExtractTypesFromRecordCode sig
+  ExtractTypesFromRecordCode '[]             = '[]
+  ExtractTypesFromRecordCode ( '(_, a) : r ) = a : ExtractTypesFromRecordCode r
 
+-- | Extracts all the field labels from a record code.
 type family ExtractLabelsFromRecordCode (r :: RecordCode) :: [FieldLabel] where
-  ExtractLabelsFromRecordCode '[]               = '[]
-  ExtractLabelsFromRecordCode ( '(l, _) : sig ) = l : ExtractLabelsFromRecordCode sig
+  ExtractLabelsFromRecordCode '[]             = '[]
+  ExtractLabelsFromRecordCode ( '(l, _) : r ) = l : ExtractLabelsFromRecordCode r
 
+-- | Given a list of labels and types, recombines them into a record code.
+--
+-- An important aspect of this function is that it is defined by induction
+-- on the list of types, and forces the list of field labels to be at least
+-- as long.
+--
 type family RecombineRecordCode (ls :: [FieldLabel]) (ts :: [Type]) :: RecordCode where
   RecombineRecordCode _ '[]       = '[]
   RecombineRecordCode ls (t : ts) = '(Head ls, t) : RecombineRecordCode (Tail ls) ts
 
-type ValidRecordCode sig xs =
-  ( ExtractTypesFromRecordCode sig ~ xs
-  , RecombineRecordCode (ExtractLabelsFromRecordCode sig) xs ~ sig
-  )
+--------------------------------------------------------------------------
+-- Conversion between a type and its record representation.
+--------------------------------------------------------------------------
 
-type IsRecord a sig = IsRecord' a sig (GetSingleton (Code a))
-
-type IsRecord' a sig xs =
-  ( Generic a, Code a ~ '[ xs ]
-  , RecordCodeOf a ~ sig, ValidRecordCode sig xs
-  )
-
-toRecord :: (IsRecord a sig) => a -> RecordRep a
+-- | Convert a value into its record representation.
+toRecord :: (IsRecord a _r) => a -> RecordRep a
 toRecord = unsafeToRecord_NP . unZ . unSOP . from
 
-toRecord_NP :: (ValidRecordCode sig xs) => NP I xs -> Record sig
+-- | Convert an n-ary product into the corresponding record
+-- representation. This is a no-op, and more efficiently
+-- implented using 'unsafeToRecord_NP'. It is included here
+-- to demonstrate that it actually is type-correct and also
+-- to make it more obvious that it is indeed a no-op.
+--
+toRecord_NP :: (ValidRecordCode r xs) => NP I xs -> Record r
 toRecord_NP Nil         = Nil
 toRecord_NP (I x :* xs) = P x :* toRecord_NP xs
 
-unsafeToRecord_NP :: (ValidRecordCode sig xs) => NP I xs -> Record sig
+-- | Fast version of 'toRecord_NP'. Not actually unsafe as
+-- long as the internal representations of 'NP' and 'Record'
+-- are not changed.
+--
+unsafeToRecord_NP :: (ValidRecordCode r xs) => NP I xs -> Record r
 unsafeToRecord_NP = unsafeCoerce
 
-fromRecord :: (IsRecord a sig) => RecordRep a -> a
+-- | Convert a record representation back into a value.
+fromRecord :: (IsRecord a r) => RecordRep a -> a
 fromRecord = to . SOP . Z . unsafeFromRecord_NP
 
-fromRecord_NP :: forall sig xs . (ValidRecordCode sig xs, SListI xs) => Record sig -> NP I xs
+-- | Convert a record representation into an n-ary product. This is a no-op,
+-- and more efficiently implemented using 'unsafeFromRecord_NP'.
+--
+-- It is also noteworthy that we let the resulting list drive the computation.
+-- This is compatible with the definition of 'RecombineRecordCode' based on
+-- the list of types.
+--
+fromRecord_NP :: forall r xs . (ValidRecordCode r xs, SListI xs) => Record r -> NP I xs
 fromRecord_NP = case sList :: SList xs of
   SNil  -> const Nil
   SCons -> \ r -> case r of
     P x :* xs -> I x :* fromRecord_NP xs
 
-unsafeFromRecord_NP :: forall sig xs . (ValidRecordCode sig xs, SListI xs) => Record sig -> NP I xs
+-- | Fast version of 'fromRecord_NP'. Not actually unsafe as
+-- long as the internal representation of 'NP' and 'Record'
+-- are not changed.
+--
+unsafeFromRecord_NP :: forall r xs . (ValidRecordCode r xs, SListI xs) => Record r -> NP I xs
 unsafeFromRecord_NP = unsafeCoerce
 
--- * Utilities
+--------------------------------------------------------------------------
+-- Utilities
+--------------------------------------------------------------------------
  
 -- | Projection of the second component of a type-level pair,
 -- wrapped in a newtype.
 --
 newtype P (p :: (a, Type)) = P (Snd p)
+  deriving (GHC.Generic)
+
+deriving instance Show a => Show (P '(l, a))
 
 -- | Type-level variant of 'snd'.
 type family Snd (p :: (a, b)) :: b where
